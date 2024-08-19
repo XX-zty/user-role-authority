b75f70640b2730737138a20f86c68ef694979e2b
XX-zty <13083006917@163.com>
2024-08-19 10:50:13 +0800

    修改前端界面

--- user-role-frontend/src/views/login/index.vue
+++ user-role-frontend/src/views/login/index.vue
@@ -37,7 +37,6 @@
         <!-- 按钮区域 -->
         <el-form-item class="btns">
           <el-button type="primary" @click="login">登录</el-button>
-          <!-- <el-button type="primary" @click="register">注册</el-button> -->
           <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
